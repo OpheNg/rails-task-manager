090eee76ee4dbbce9447acd6c65a503ff31462e6
OpheNg <ophe.ng@live.fr>
2022-07-16 11:21:42 +0200

    list controller route et view OK

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,5 @@
 class TasksController < ApplicationController
+  def list
+  end
+
 end
--- db/migrate/20220716082619_create_tasks.rb
+++ db/migrate/20220716082619_create_tasks.rb
@@ -3,7 +3,7 @@ class CreateTasks < ActiveRecord::Migration[6.1]
     create_table :tasks do |t|
       t.string :title
       t.text :details
-      t.false :completed,default
+      t.boolean :completed, default: false
 
       t.timestamps
     end
