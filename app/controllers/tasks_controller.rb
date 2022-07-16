531b85c3597a56332cca543f62ba569714386344
OpheNg <ophe.ng@live.fr>
2022-07-16 12:16:41 +0200

    edit view ok

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -11,4 +11,13 @@ class TasksController < ApplicationController
     @task = Task.new
   end
 
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to tasks_path
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
 end
